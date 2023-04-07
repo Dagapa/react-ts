9d86b1b17e27a3ed2a110ac576177988121432c8
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-04-06 22:02:46 -0500

    make a changes on the random function

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -2,7 +2,8 @@
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random}.jpg`;
+  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
+  console.log(image);
 
-  return <img width={320} height="auto" className="rounded" src={image} />;
+  return <img width={320} height="auto" src={image} />;
 };
