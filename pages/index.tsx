b87bfeb897491f705062d2eaa2b0e84522c98875
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-04-06 22:00:33 -0500

    get a picture from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { RandomFox } from "@/components/RandomFox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <RandomFox />
       </main>
     </>
   );
